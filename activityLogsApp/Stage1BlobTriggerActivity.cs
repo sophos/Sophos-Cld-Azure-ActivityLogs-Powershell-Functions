3186099eaf56d25598a870ba6b2a718cfa815fab
Aashish Gupta <aashish.gupta@sophos.com>
2025-02-05 13:51:45 +0530

    reverting stage code to master

--- activityLogsApp/Stage1BlobTriggerActivity.cs
+++ activityLogsApp/Stage1BlobTriggerActivity.cs
@@ -9,7 +9,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-using Azure.Identity;
+
 
 namespace NwNsgProject
 {
@@ -19,14 +19,20 @@ namespace NwNsgProject
 
         [FunctionName("Stage1BlobTriggerActivity")]
         public static async Task Run(
-            [BlobTrigger("%blobContainerNameActivity%/resourceId=/SUBSCRIPTIONS/{subId}/y={blobYear}/m={blobMonth}/d={blobDay}/h={blobHour}/m={blobMinute}/PT1H.json")] AppendBlobClient myBlobActivity,
-            [Queue("activitystage1")] ICollector<Chunk> outputChunksActivity,
+            [BlobTrigger("%blobContainerNameActivity%/resourceId=/SUBSCRIPTIONS/{subId}/y={blobYear}/m={blobMonth}/d={blobDay}/h={blobHour}/m={blobMinute}/PT1H.json", Connection = "nsgSourceDataConnection")] AppendBlobClient myBlobActivity,
+            [Queue("activitystage1", Connection = "AzureWebJobsStorage")] ICollector<Chunk> outputChunksActivity,
             string subId, string blobYear, string blobMonth, string blobDay, string blobHour, string blobMinute,
             ILogger log)
         {
             try
             {
                 log.LogInformation("starting");
+                string nsgSourceDataAccount = Util.GetEnvironmentVariable("nsgSourceDataAccount");
+                if (nsgSourceDataAccount.Length == 0)
+                {
+                    log.LogError("Value for nsgSourceDataAccount is required.");
+                    throw new System.ArgumentNullException("nsgSourceDataAccount", "Please provide setting.");
+                }
 
                 string blobContainerName = Util.GetEnvironmentVariable("blobContainerNameActivity");
                 if (blobContainerName.Length == 0)
@@ -37,29 +43,9 @@ namespace NwNsgProject
 
                 var blobDetails = new BlobDetailsActivity(subId, blobYear, blobMonth, blobDay, blobHour, blobMinute);
 
-                // Authenticate using Managed Identity
-                var credential = new DefaultAzureCredential();
-                string subscriptionIds = Util.GetEnvironmentVariable("subscriptionIds");
-                if (string.IsNullOrEmpty(subscriptionIds))
-                {
-                    log.LogError("Value for subscriptionIds is required.");
-                    throw new ArgumentNullException("subscriptionIds", "SubscriptionId is not found in environment settings.");
-                }
-                string customerId = Util.GetEnvironmentVariable("customerId");
-                if (string.IsNullOrEmpty(customerId))
-                {
-                    log.LogError("Value for customerId is required.");
-                    throw new ArgumentNullException("customerId", "customerId is not found in environment settings..");
-                }
-                log.LogInformation("POC | value for subscriptionIds: {subscriptionIds}", subscriptionIds);
-
-                log.LogInformation("POC | value for customerId: {customerId}", customerId);
-
-                string storageAccountName = "lavidact" + subscriptionIds.Replace("-", "").Substring(0, 8) + customerId.Replace("-", "").Substring(0, 8);
-                log.LogInformation("POC | value for storageAccountName: {StorageAccountName}", storageAccountName);
-
-                string tableEndpoint = $"https://{storageAccountName}.table.core.windows.net/";
-                TableClient tableClient = new TableClient(new Uri(tableEndpoint), "activitycheckpoints", credential);
+                string storageConnectionString = Util.GetEnvironmentVariable("AzureWebJobsStorage");
+                // Create a TableClient instance
+                TableClient tableClient = new TableClient(storageConnectionString, "activitycheckpoints");
                 // Create table if not exist
                 await tableClient.CreateIfNotExistsAsync();
 
@@ -70,16 +56,16 @@ namespace NwNsgProject
                 var blobProperties = await myBlobActivity.GetPropertiesAsync();
                 long blobSize = blobProperties.Value.ContentLength;
                 long chunklength = blobSize - checkpoint.StartingByteOffset;
-                if (chunklength > 10)
+                if(chunklength >10)
                 {
                     Chunk newchunk = new Chunk
-                    {
-                        BlobName = blobContainerName + "/" + myBlobActivity.Name,
-                        Length = chunklength,
-                        LastBlockName = "",
-                        Start = checkpoint.StartingByteOffset,
-                        BlobAccountConnectionName = "ManagedIdentity"
-                    };
+                        {
+                            BlobName = blobContainerName + "/" + myBlobActivity.Name,
+                            Length = chunklength,
+                            LastBlockName = "",
+                            Start = checkpoint.StartingByteOffset,
+                            BlobAccountConnectionName = nsgSourceDataAccount
+                        };
 
                     checkpoint.PutCheckpointActivity(tableClient, blobSize);
                     outputChunksActivity.Add(newchunk);
@@ -87,7 +73,7 @@ namespace NwNsgProject
             }
             catch (Exception e)
             {
-                log.LogError(e, "Function Stage1BlobTriggerActivity is failed to process request");
+                 log.LogError(e, "Function Stage1BlobTriggerActivity is failed to process request");
             }
 
         }
